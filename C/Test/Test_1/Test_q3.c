2e86e983999abc72651e3dae8e628e16f95b6eca
varunkawade <varunkawade443@gmail.com>
2025-10-04 17:41:32 +0530

    Update Test_q3.c

--- C/Test/Test_1/Test_q3.c
+++ C/Test/Test_1/Test_q3.c
@@ -1,3 +1,7 @@
+// Print last 2 digit of number.
+// Eg. i/p: 223410
+// o/p: 10
+
 #include <stdio.h>
 
 int main() {
@@ -13,5 +17,6 @@ printf("Number = %d\n", num);
 printf("Last two digits = %02d\n", last_two);
 
 return 0;
+
 
 }
