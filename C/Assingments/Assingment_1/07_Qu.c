dfa9c652967d72e8ea3807421ee8a643bcaea030
varunkawade <varunkawade443@gmail.com>
2025-10-04 17:14:50 +0530

    Update 07_Qu.c

--- C/Assingments/Assingment_1/07_Qu.c
+++ C/Assingments/Assingment_1/07_Qu.c
@@ -1,3 +1,8 @@
+// Calculating total salary based on basic. If basic <=5000 da, ta and hra will be
+// 10%,20% and 25% respectively otherwise da, ta and hra will be 15%,25% and 30%
+// respectively.
+
+
 #include<stdio.h>
 
 int main(){
@@ -17,4 +22,5 @@ int main(){
 	printf("%2lf",total);
 	
     return 0;
+
 }
