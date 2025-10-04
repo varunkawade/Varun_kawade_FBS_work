9ea0248adbac8e1d6308a7026876270b77353020
varunkawade <varunkawade443@gmail.com>
2025-10-04 15:14:37 +0530

    Add files via upload

---
+++ C/Test/Test_1/Test_q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+int year = 2024; // hardcoded value
+
+if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+
+printf("%d is a Leap Year\n", year);
+
+else
+
+printf("%d is NOT a Leap Year\n", year);
+
+return 0;
+
+}
