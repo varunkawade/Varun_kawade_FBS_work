aa6c7800f481800dd95d355a07252f9a952e37c8
varunkawade <varunkawade443@gmail.com>
2025-10-27 16:16:50 +0530

    Refactor strong number check into a single function

--- C/Assingments/Assingment_6/Assignment3_into_Funtion/Q7_ass3funtion.c
+++ C/Assingments/Assingment_6/Assignment3_into_Funtion/Q7_ass3funtion.c
@@ -1,37 +1,37 @@
-
 #include<stdio.h>
 
-void strong(int no);
+void strong();   
 
 void main()
 {
-	int no;
-  	printf("Enter no\n");
-	scanf("%d",&no);
-	strong(no);	
+    strong();    
 }
 
-void strong(int no)
+void strong()  
 {
-	int temp=no, rem , sum=0, fact;
-	
-	while(no>0)
-	{
-		rem = no%10;
-		fact=1;
-	
-    	for(int i=1; i<=rem; i++)
-	    {
-		  fact=fact*i;
-		}
-    	sum=sum+fact;
-    	no=no/10;
-	}
-	
-	if(temp==sum)
-	   printf("strong");
+    int no, temp, rem, sum = 0, fact;
+
+    printf("Enter number: ");
+    scanf("%d", &no);
+
+    temp = no;
+
+    while(no > 0)
+    {
+        rem = no % 10;
+        fact = 1;
+
+        for(int i = 1; i <= rem; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        no = no / 10;
+    }
+
+    if(temp == sum)
+        printf("Strong number");
     else
-      printf("not strong");
-	
-	
+        printf("Not strong number");
 }
